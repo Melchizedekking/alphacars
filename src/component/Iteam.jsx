422710bba6051f745917486ffd8d7bb4213ceb4f
Melchizedekking <john.iwudibie@gmail.com>
2024-12-23 19:05:56 +0100

    finished home page media query

--- src/component/Iteam.jsx
+++ src/component/Iteam.jsx
@@ -1,20 +1,26 @@
 import { Link } from 'react-router-dom'
 import './Iteam.css'
+import { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
  
  const Iteam = (props) => {
+  const {addToCart} = useContext(ShopContext)
    return (
     <>
      
 
      <div className='item' key={props.id}>
     <Link to={`/product/${props.id}`}>
-    <img onClick={window.scrollTo(0,0)} src={props.image} alt="" width={'350px'} />
+    <img onClick={window.scrollTo(0,0)} src={props.image} alt="" className='item-img' />
      </Link>
       <h3>{props.name}</h3>
       <p>{props.description}</p>
       <div className='item-prices'>
         <div className='item-price-new'>${props.newPrice}</div>
         <div className='item-price-old'>${props.oldPrice}</div>
+      </div>
+      <div className='item-buttons'>
+        <button onClick={()=>{addToCart(props.id)}}>Add to cart</button>
       </div>
 
 
